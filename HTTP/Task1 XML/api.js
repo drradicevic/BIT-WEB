e3a802299ce775d6f2377b61e76039d071f4d383
drradicevic <drradicevic@gmail.com>
2021-06-30 10:56:25 +0200

    HTTP XML task add feature

--- HTTP/Task1 XML/api.js
+++ HTTP/Task1 XML/api.js
@@ -1,6 +1,8 @@
-var endpoint = "http://www.geoplugin.net/xml.gp?ip=164.152.189.25";
+var endpoint = "http://www.geoplugin.net/xml.gp?ip=";
 
 var $countryDisplay = document.querySelector("#country");
+var $continentDisplay = document.querySelector("#continent");
+var $sityDisplay = document.querySelector("#sity");
 var $submit = document.querySelector("#submit");
 var $ipInput = document.querySelector("#ip");
 
@@ -12,8 +14,14 @@ newRequest.onload = function () {
   if (newRequest.status >= 200 && newRequest.status < 300) {
     var response = newRequest.responseXML;
     var country = response.querySelector("geoplugin_countryName").textContent;
+    var continent = response.querySelector(
+      "geoplugin_continentName"
+    ).textContent;
+    var sity = response.querySelector("geoplugin_city").textContent;
 
-    $countryDisplay.textContent = country;
+    $countryDisplay.textContent = "Country: " + country;
+    $continentDisplay.textContent = "Continent: " + continent;
+    $sityDisplay.textContent = "Sity: " + sity;
   }
 };
 
